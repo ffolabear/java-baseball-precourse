362e8a1c3ecd46b2b3e1e45284442ac59fdc4c45
ffolabear <ffolabear@gmail.com>
2022-10-24 19:14:16 +0900

    initial commit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+
+
+
     }
 }
